eaae649517b2ce3d85561c10e1960915cb0dae37
Ben Hofer <06hofben@rgtfo-me.it>
2025-03-11 13:01:01 +0100

    Kommentierung

--- NINA_Driver_V1/SetupDialog_Form.cs
+++ NINA_Driver_V1/SetupDialog_Form.cs
@@ -1,15 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace NINA_Driver_V1
 {
-    //SetupDialog_WindowsForms um die ersten Einstellungen festzulegen
+    [ComVisible(false)] //Darf nicht für COM sichtbar sein, da sonst der COM-Compiler einen Fehler wirft.
+
+    //Dieser Code ist ein Windowsform (von KI generiert) und wird für die Einstellungen des Treibers verwendet. Da ich es nicht benötige,
+    //kann man nicht wirklich etwas einstellen
     public partial class SetupDialogForm : Form
     {
+        public string SettingValue1 { get; set; }
+        public string SettingValue2 { get; set; }
+
         public SetupDialogForm()
         {
             InitializeComponent();
@@ -17,80 +20,74 @@ namespace NINA_Driver_V1
 
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxSetting1 = new System.Windows.Forms.TextBox();
+            this.textBoxSetting2 = new System.Windows.Forms.TextBox();
             this.buttonOK = new System.Windows.Forms.Button();
             this.buttonCancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // textBoxSetting1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Label:";
+            this.textBoxSetting1.Location = new System.Drawing.Point(12, 12);
+            this.textBoxSetting1.Name = "textBoxSetting1";
+            this.textBoxSetting1.Size = new System.Drawing.Size(260, 20);
+            this.textBoxSetting1.TabIndex = 0;
             // 
-            // textBox1
+            // textBoxSetting2
             // 
-            this.textBox1.Location = new System.Drawing.Point(65, 10);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 1;
+            this.textBoxSetting2.Location = new System.Drawing.Point(12, 38);
+            this.textBoxSetting2.Name = "textBoxSetting2";
+            this.textBoxSetting2.Size = new System.Drawing.Size(260, 20);
+            this.textBoxSetting2.TabIndex = 1;
             // 
             // buttonOK
             // 
-            this.buttonOK.Location = new System.Drawing.Point(16, 50);
+            this.buttonOK.Location = new System.Drawing.Point(116, 64);
             this.buttonOK.Name = "buttonOK";
             this.buttonOK.Size = new System.Drawing.Size(75, 23);
             this.buttonOK.TabIndex = 2;
             this.buttonOK.Text = "OK";
             this.buttonOK.UseVisualStyleBackColor = true;
-            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            this.buttonOK.Click += new System.EventHandler(this.ButtonOK_Click);
             // 
             // buttonCancel
             // 
-            this.buttonCancel.Location = new System.Drawing.Point(97, 50);
+            this.buttonCancel.Location = new System.Drawing.Point(197, 64);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(75, 23);
             this.buttonCancel.TabIndex = 3;
             this.buttonCancel.Text = "Cancel";
             this.buttonCancel.UseVisualStyleBackColor = true;
-            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            this.buttonCancel.Click += new System.EventHandler(this.ButtonCancel_Click);
             // 
             // SetupDialogForm
-            //
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 101);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonOK);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSetting2);
+            this.Controls.Add(this.textBoxSetting1);
             this.Name = "SetupDialogForm";
             this.ResumeLayout(false);
             this.PerformLayout();
         }
 
-        private void buttonOK_Click(object sender, EventArgs e)
+        private void ButtonOK_Click(object sender, EventArgs e)
         {
-            // Speichern Sie die Einstellungen hier
+            this.SettingValue1 = this.textBoxSetting1.Text;
+            this.SettingValue2 = this.textBoxSetting2.Text;
             this.DialogResult = DialogResult.OK;
             this.Close();
         }
 
-        private void buttonCancel_Click(object sender, EventArgs e)
+        private void ButtonCancel_Click(object sender, EventArgs e)
         {
             this.DialogResult = DialogResult.Cancel;
             this.Close();
         }
-        public string SettingValue
-        {
-            get { return textBox1.Text; }
-            set { textBox1.Text = value; }
-        }
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxSetting1;
+        private System.Windows.Forms.TextBox textBoxSetting2;
         private System.Windows.Forms.Button buttonOK;
         private System.Windows.Forms.Button buttonCancel;
     }
--- NINA_Driver_V1/Starwatcher_Driver.cs
+++ NINA_Driver_V1/Starwatcher_Driver.cs
@@ -1,19 +1,57 @@
 ﻿using System;
 using System.Collections;
+using System.IO;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Threading.Tasks.Sources;
 using System.Windows.Forms;
 using ASCOM.DeviceInterface;
 using ASCOM.Utilities;
 
+//Dieser Treiber implementiert die von der ASCOM-Website heruntergeladene IDomeV2-Schnittstelle in einer Klasse namens Starwatcher_Driver.
+//Die Klasse implementiert die Methoden und Eigenschaften der IDomeV2-Schnittstelle und verwendet einen TraceLogger für Debugging-Informationen.
+//Dafür wird die ASCOM.Utilities und ASCOM.Deviceinterface verwendet, die in der ASCOM-Plattform enthalten ist.
+
 namespace NINA_Driver_V1
 {
-    [Guid("6ac65995-6304-4935-aa0c-aaeb0ae4c5cc")] // benötigt einen einzigartigen GUID (Globally Unique Identifier)
-    [ClassInterface(ClassInterfaceType.None)] // keine automatische COM-Schnittstelle generieren
+    //Die folgenden Deklarationen und Attribute sind notwendig, um die Klasse als COM-Objekt zu registrieren und sie für
+    //ASCOM sichtbar zu machen.
+    //Ein COM-Objekt ist eine Technologie zur Interprozesskommunikation, die es ermöglicht Programmen und Komponenten, unabhängig
+    //von der Programmiersprache, aufeinander zuzugreifen.
+    [Guid("b535bec2-a119-4b89-b727-84add9028406")] //GUID: Eine eindeutige Kennung für die COM-Klasse
+    [ClassInterface(ClassInterfaceType.None)]      //ClassInterface: Gibt an, dass keine automatische Schnittstelle von Windows erstellt wird 
+    [ProgId("Driver_V1.Starwatcher_BenHfr")]       //Progid: Eindeutiger Name für die COM-Klasse
+    [ComVisible(true)]                             //Macht die Klasse für COM sichtbar (WICHTIG). Muss in Assembly.cs auf false gesetzt werden
+                                                   //damit nicht das komplette Programm für den COM-Compiler sichtbar ist.
     public class Starwatcher_Driver : IDomeV2
     {
-        private const string driverID = "ASCOM.NINA_Driver_V1.Starwatcher_Driver";
-        private const string driverDescription = "Starwatcher Dome Driver";
+        // Konsistente ID verwenden
+        private const string driverID = "Driver_V1.Starwatcher_BenHfr";
+        private const string driverDescription = "Starwatcher Maturaprojekt 24/25";
+        private const string driverName = "Starwatcher Ben Hofer";
 
+        // TraceLogger für Debugging
+        private TraceLogger tl;
+
+        // Konstruktor
+        public Starwatcher_Driver()
+        {
+            // TraceLogger für Debugging
+            //Schreibt alle Aktionen auf und speichert sie in einem Logfile in "C:\...\...\AppData\Local\ASCOM\Logs"
+            string logDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ASCOM", "Logs", DateTime.Now.ToString("yyyy-MM-dd"));
+            if (!Directory.Exists(logDirectory))
+            {
+                Directory.CreateDirectory(logDirectory);
+            }
+            string logFilePath = Path.Combine(logDirectory, "Starwatcher_log.txt");
+            tl = new TraceLogger(logFilePath, "Starwatcher");
+            tl.Enabled = true;
+            tl.LogMessage("Starwatcher_Driver", "Initialisiert");
+        }
+
+        //Registrierung-----------------------------------------------------------------
+        //Diese Methoden werden benötigt, um den Treiber für ASCOM zu registrieren und zu de-registrieren.
+        //Dies ermöglicht es ASCOM,, den Treiber zu erkennen und zu verwenden
         [ComRegisterFunction]
         public static void RegisterASCOM(Type t)
         {
@@ -26,6 +64,9 @@ namespace NINA_Driver_V1
             Registration.Unregister(t);
         }
 
+        [ComVisible(false)]
+        //Die Registration-Klasse enthält die eigentlichen Registrierungs- und De-Registrierungslogik; sie ist in den Developer-Tools
+        //Von ASCOM enthalten und wird in den Registrierungsmethoden aufgerufen.
         public static class Registration
         {
             public static void Register(Type t)
@@ -33,9 +74,10 @@ namespace NINA_Driver_V1
                 using (var profile = new Profile())
                 {
                     profile.DeviceType = "Dome";
-                    profile.Register(driverID, driverDescription);
+                    profile.Register(driverID, "Starwatcher Ben Hofer");
                 }
             }
+
 
             public static void Unregister(Type t)
             {
@@ -47,44 +89,60 @@ namespace NINA_Driver_V1
             }
         }
 
+        //Ersteinstellungen-----------------------------------------------------------------
+        private string settingValue1;
+        private string settingValue2;
+        //Der SetupDialog ermglicht es dem Benutzer, Einstellungen für den Treiber zu konfigurieren.
+        //Diese Einstellungen werden in der ASCOM-Profile-Registry gespeichert und können von der ASCOM-Plattform abgerufen werden.
+        //SetupDialog ist notwendig um von der ASCOM-Plattform erkannt zu werden, auch wenn es für dieses Projekt nicht benötigt wird, da
+        //mein Treiber nur für eine einzige Kuppel geschrieben wird, welche nicht umgestellt wird.
         public void SetupDialog()
         {
             using (var form = new SetupDialogForm())
-                using (var profile = new Profile())
-                {
-                    profile.DeviceType = "Dome";
-                    form.SettingValue = profile.GetValue(driverID, "SettingName", string.Empty, "DefaultSettingValue");
-                }
+            using (var profile = new Profile())
+            {
+                profile.DeviceType = "Dome";
+                // Einstellungen laden
+                form.SettingValue1 = profile.GetValue(driverID, "SettingName1", "", "DefaultValue1");
+                form.SettingValue2 = profile.GetValue(driverID, "SettingName2", "", "DefaultValue2");
 
                 if (form.ShowDialog() == DialogResult.OK)
                 {
-                    // Speichern Sie die Einstellungen aus dem Dialogfenster in der ASCOM-Profilklasse
-                    using (var profile = new Profile())
-                    {
-                        profile.DeviceType = "Dome";
-                        profile.WriteValue(driverID, "SettingName", form.SettingValue);
-                    }
+                    // Einstellungen speichern
+                    profile.WriteValue(driverID, "SettingName1", form.SettingValue1);
+                    profile.WriteValue(driverID, "SettingName2", form.SettingValue2);
                 }
             }
-
+        }
+        //----------------------------------------------------------------------------------------
         // Connect & Disconnect
-        private bool connected = false;
+        private bool connected;
         public bool Connected
         {
             get { return connected; }
-            set { connected = value; }
+            set
+            {
+                if (value) Connect();
+                else Disconnect();
+            }
         }
 
         public void Connect()
         {
+            using (var profile = new Profile())
+            {
+                profile.DeviceType = "Dome";
+                settingValue1 = profile.GetValue(driverID, "SettingName1", "", "DefaultValue1");
+                settingValue2 = profile.GetValue(driverID, "SettingName2", "", "DefaultValue2");
+            }
             connected = true;
-            Console.WriteLine("Connected to Starwatcher");
+            tl.LogMessage("Connect","Connected to Starwatcher");
         }
 
         public void Disconnect()
         {
             connected = false;
-            Console.WriteLine("Disconnected from Starwatcher");
+            tl.LogMessage("Disconnect","Disconnected from Starwatcher");
         }
 
         // Park
@@ -93,23 +151,48 @@ namespace NINA_Driver_V1
         {
             get { return atPark; }
         }
-
+        public bool CanPark
+        {
+            get { return true; }
+        }
         public void Park()
         {
+            //Parken wird simuliert
             atPark = true;
-            Console.WriteLine("Parked");
+            tl.LogMessage("Park", "Parked");
         }
 
         // Slew to Azimuth
+
+       /* public double Azimuth
+        {
+            get
+            {
+                // Implementieren Sie die echte Azimuth-Abfrage
+                return ReadAzimuthFromHardware();
+            }
+        }*/
         public void SlewToAzimuth(double Azimuth)
         {
-            Console.WriteLine("Slewing to Azimuth: " + Azimuth);
+            tl.LogMessage("SlewToAzimuth", "Slewing to Azimuth: " + Azimuth); 
         }
+
+        //Luke öffnen und schließen
+
+        public void OpenShutter()
+        {
+            tl.LogMessage("OpenShutter", "Open Shutter");
+        }
+        public void CloseShutter()
+        {
+            tl.LogMessage("CloseShutter", "Close Shutter");
+        }
+
 
         // Noch zu Implementieren
         public void AbortSlew()
         {
-            Console.WriteLine("Abort Slew");
+            tl.LogMessage("AbortSlew", "Abort Slew");
         }
 
         public double Altitude
@@ -132,10 +215,7 @@ namespace NINA_Driver_V1
             get { return false; }
         }
 
-        public bool CanPark
-        {
-            get { return true; }
-        }
+        
 
         public bool CanSetAltitude
         {
@@ -154,7 +234,7 @@ namespace NINA_Driver_V1
 
         public bool CanSetShutter
         {
-            get { return false; }
+            get { return true; }
         }
 
         public bool CanSlave
@@ -167,24 +247,18 @@ namespace NINA_Driver_V1
             get { return true; }
         }
 
-        public void CloseShutter()
-        {
-            Console.WriteLine("Close Shutter");
-        }
+        
 
         public void FindHome()
         {
-            Console.WriteLine("Find Home");
+            tl.LogMessage("FindHome", "Find Home");
         }
 
-        public void OpenShutter()
-        {
-            Console.WriteLine("Open Shutter");
-        }
+
 
         public void SetPark()
         {
-            Console.WriteLine("Set Park");
+            tl.LogMessage("SetPark", "Set Park");
         }
 
         public ShutterState ShutterStatus
@@ -195,12 +269,12 @@ namespace NINA_Driver_V1
         public bool Slaved
         {
             get { return false; }
-            set { Console.WriteLine("Set Slaved: " + value); }
+            set { tl.LogMessage("Slaved", "Set Slaved: " + value); }
         }
 
         public void SlewToAltitude(double Altitude)
         {
-            Console.WriteLine("Slew to Altitude: " + Altitude);
+            tl.LogMessage("SlewToAltitude", "Slew to Altitude: " + Altitude);
         }
 
         public bool Slewing
@@ -210,7 +284,7 @@ namespace NINA_Driver_V1
 
         public void SyncToAzimuth(double Azimuth)
         {
-            Console.WriteLine("Sync to Azimuth: " + Azimuth);
+            tl.LogMessage("SyncToAzimuth", "Sync to Azimuth: " + Azimuth);
         }
 
         public string Action(string ActionName, string ActionParameters)
@@ -236,6 +310,8 @@ namespace NINA_Driver_V1
         public void Dispose()
         {
             // Bereinigen Sie alle Ressourcen hier
+            tl.Enabled = false;
+            tl.Dispose();
         }
 
         public string Description
